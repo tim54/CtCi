9483249a35bfd2105957716d6db01f1de627352b
Kovalevsky Artem-RCFT46 <Artem.Kovalevsky@motorolasolutions.com>
2016-12-02 17:20:28 +0300

    Q1_02_Check_Permutation
    Change-Id: Iea54f58163e040d290888b47bd2c75dbf5a63fbe

--- Java/Ch 01. Arrays and Strings/Q1_02_Check_Permutation/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_02_Check_Permutation/QuestionB.java
@@ -1,6 +1,7 @@
 package Q1_02_Check_Permutation;
 
-public class QuestionB {	
+public class QuestionB {
+	private static int ALPHABET_NUM = 62;
 	public static boolean permutation(String s, String t) {
 		if (s.length() != t.length()) return false; // Permutations must be same length
 		
@@ -19,12 +20,38 @@ public class QuestionB {
 		return true; // letters array has no negative values, and therefore no positive values either
 	}
 	
+	public static boolean myPermutation(String s, String t) {
+		if (s.length() != t.length()) return false;
+		
+		int[] masS = new int[ALPHABET_NUM];
+		int[] masT = new int[ALPHABET_NUM];
+		
+		for (int i = 0; i < s.length(); i++){
+			int elemNumS = s.charAt(i) - 'a';
+			int elemNumT = t.charAt(i) - 'a';
+			
+			if (elemNumS < 0 || elemNumS > ALPHABET_NUM) throw new ArrayIndexOutOfBoundsException();
+			if (elemNumT < 0 || elemNumT > ALPHABET_NUM) throw new ArrayIndexOutOfBoundsException();
+			
+			masS[elemNumS]++;
+			masT[elemNumT]++;
+		}
+		
+		for (int i = 0; i < ALPHABET_NUM; i++) {
+			if (masS[i] == masT[i]) continue;
+			else return false;
+		}
+
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
 		for (String[] pair : pairs) {
 			String word1 = pair[0];
 			String word2 = pair[1];
-			boolean anagram = permutation(word1, word2);
+			// boolean anagram = permutation(word1, word2);
+			boolean anagram = myPermutation(word1, word2);
 			System.out.println(word1 + ", " + word2 + ": " + anagram);
 		}
 	}
